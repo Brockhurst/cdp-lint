220927bc4b5c0a551a3ef09e10a1c90552a7325a
Mykyta_Holkin@epam.com <Mykyta_Holkin@epam.com>
2016-12-27 23:01:37 +0200

    test

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../less/all.less';
 import * as padder from '../../node_modules/jsmp-infra-padder/lib';
 
-
 $('.btn-primary').click(function () {
     console.log(padder.padString('abc', 'both', 8)); // eslint-disable-line no-console
 });
